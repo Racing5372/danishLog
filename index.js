93ad8686e60e372d362d75e0bddcb76fc2a17c8d
Andreas Risager <andreasbarkov@gmail.com>
2020-10-21 10:55:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function danishLog(string){
+    var reset = "\x1b[0m",
+    colors = ["\x1b[31m", "\x1b[37m"]
+    output = "";
+
+    for(let i=0; i<string.length; i++){
+        output += colors[i % colors.length] + string[i] + reset;
+    }
+    console.log(output);
+}
+module.exports = danishLog;
